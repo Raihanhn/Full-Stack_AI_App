e8118efd19e103b8493469d026175b081a315f41
Raihan <raihanhn20@gmail.com>
2024-02-29 01:22:28 +0600

    sidebar added

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
-  return <aside className="sidebar">Sidebar</aside>;
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link className="sidebar-logo" href="/">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
