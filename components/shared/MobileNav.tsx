1172556cb2b07878c0835f3c2603a263d1a2a01c
Raihan <raihanhn20@gmail.com>
2024-03-01 19:12:44 +0600

    mobilenav completed

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sheet,
   SheetContent,
@@ -6,11 +7,15 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
@@ -24,7 +29,58 @@ const MobileNav = () => {
       <nav className="flex gap-2 ">
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <Image
+                src="/assets/images/logo-text.svg"
+                alt="logo"
+                width={152}
+                height={23}
+              />
+              <ul className="sidebar-nav_elements">
+                {navLinks.map((link) => {
+                  const isActive = link.route === pathname;
+
+                  return (
+                    <li
+                      key={link.route}
+                      className={`sidebar-nav_element group ${
+                        isActive
+                          ? "bg-purple-gridient text-purple-500"
+                          : "text-gray-700"
+                      }`}
+                    >
+                      <Link className="sidebar-link" href={link.route}>
+                        <Image
+                          src={link.icon}
+                          alt="logo"
+                          width={24}
+                          height={24}
+                          className={`${isActive && "fill-purple-500 "}`}
+                        />
+                        {link.label}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </SheetContent>
+          </Sheet>
         </SignedIn>
+        <SignedOut>
+          <Button asChild className="button bg-purple-gradient bg-cover">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
       </nav>
     </header>
   );
